f5d519e6ac55a7526cfebb5c4e9e2c72db6f0c6b
xstars1505 <baminh.letran@gmail.com>
2018-09-06 10:52:11 +0700

    first commit

---
+++ client/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app
+    >
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+    
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ client/src/store.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import feathersVuex from 'feathers-vuex';
+import feathersClient from './feathers-client';
+
+const { 
+  auth, 
+  FeathersVuex 
+} = feathersVuex(feathersClient, { idField: '_id' });
+
+Vue.use(Vuex)
+Vue.use(FeathersVuex)
+
+export default new Vuex.Store({
+  plugins: [
+    // Setup the auth plugin.
+    auth({ userService: 'users' })
+  ]
+});
